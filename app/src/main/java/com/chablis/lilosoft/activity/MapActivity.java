a2db3e8f068440a68729411d06a8adac536f769f
chablis <windychablis@hotmail.com>
2017-03-03 15:03:23 +0800

    增加分组列表和字母排序

--- app/src/main/java/com/chablis/lilosoft/activity/MapActivity.java
+++ app/src/main/java/com/chablis/lilosoft/activity/MapActivity.java
@@ -3,6 +3,7 @@ package com.chablis.lilosoft.activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Toast;
 
@@ -18,9 +19,18 @@ import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.map.TextureMapView;
 import com.baidu.mapapi.model.LatLng;
 import com.chablis.lilosoft.R;
+import com.chablis.lilosoft.adapter.FriendsAdapter;
 import com.chablis.lilosoft.base.BaseActivity;
 import com.chablis.lilosoft.base.BaseFragment;
+import com.chablis.lilosoft.widget.BladeView;
+import com.chablis.lilosoft.widget.PinnedHeaderListView;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -37,6 +47,19 @@ public class MapActivity extends BaseActivity implements BaseFragment.OnFragment
     @BindView(R.id.bmapView)
     TextureMapView bmapView;
 
+    private static final String FORMAT = "^[a-z,A-Z].*$";
+    private PinnedHeaderListView mListView;
+    private BladeView mLetter;
+    private FriendsAdapter mAdapter;
+    private String[] datas;
+    // 首字母集
+    private List<String> mSections;
+    // 根据首字母存放数据
+    private Map<String, List<String>> mMap;
+    // 首字母位置集
+    private List<Integer> mPositions;
+    // 首字母对应的位置
+    private Map<String, Integer> mIndexer;
 
     public void initMap() {
         baiduMap = bmapView.getMap();
@@ -57,7 +80,7 @@ public class MapActivity extends BaseActivity implements BaseFragment.OnFragment
         menu = new SlidingMenu(this);
         menu.setMode(SlidingMenu.RIGHT);
         // 设置触摸屏幕的模式
-        menu.setTouchModeAbove(SlidingMenu.RIGHT);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
         // 设置滑动菜单视图的宽度
         menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
         // 设置渐入渐出效果的值
@@ -68,7 +91,66 @@ public class MapActivity extends BaseActivity implements BaseFragment.OnFragment
         // 为侧滑菜单设置布局
         menu.setMenu(R.layout.menu_map);
 
+        initData();
+        // TODO Auto-generated method stub
+        mListView = (PinnedHeaderListView) findViewById(R.id.pinnedListView);
+        mLetter = (BladeView) findViewById(R.id.friends_myletterlistview);
+        mLetter.setOnItemClickListener(new BladeView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(String s) {
+                if (mIndexer.get(s) != null) {
+                    mListView.setSelection(mIndexer.get(s));
+                }
+            }
+        });
+        mAdapter = new FriendsAdapter(this, datas, mSections, mPositions);
+        mListView.setAdapter(mAdapter);
+        mListView.setOnScrollListener(mAdapter);
+        mListView.setPinnedHeaderView(LayoutInflater.from(this).inflate(
+                R.layout.listview_head, mListView, false));
     }
+
+
+    private void initData() {
+        datas = getResources().getStringArray(R.array.countries);
+        mSections = new ArrayList<String>();
+        mMap = new HashMap<String, List<String>>();
+        mPositions = new ArrayList<Integer>();
+        mIndexer = new HashMap<String, Integer>();
+
+        for (int i = 0; i < datas.length; i++) {
+            String firstName = datas[i].substring(0, 1);
+            if (firstName.matches(FORMAT)) {
+                if (mSections.contains(firstName)) {
+                    mMap.get(firstName).add(datas[i]);
+                } else {
+                    mSections.add(firstName);
+                    List<String> list = new ArrayList<String>();
+                    list.add(datas[i]);
+                    mMap.put(firstName, list);
+                }
+            } else {
+                if (mSections.contains("#")) {
+                    mMap.get("#").add(datas[i]);
+                } else {
+                    mSections.add("#");
+                    List<String> list = new ArrayList<String>();
+                    list.add(datas[i]);
+                    mMap.put("#", list);
+                }
+            }
+        }
+
+        Collections.sort(mSections);
+        int position = 0;
+        for (int i = 0; i < mSections.size(); i++) {
+            mIndexer.put(mSections.get(i), position);// 存入map中，key为首字母字符串，value为首字母在listview中位置
+            mPositions.add(position);// 首字母在listview中位置，存入list中
+            position += mMap.get(mSections.get(i)).size();// 计算下一个首字母在listview的位置
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
