a89a18213c7ed65f6cc2fd13eb0bfcff029d4fed
chablis <windychablis@hotmail.com>
2017-03-08 17:11:22 +0800

    增加表单的搜索功能

--- app/src/main/java/com/chablis/lilosoft/activity/TableListActivity.java
+++ app/src/main/java/com/chablis/lilosoft/activity/TableListActivity.java
@@ -6,7 +6,11 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.TextView;
 
@@ -23,6 +27,7 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class TableListActivity extends BaseActivity {
 
@@ -32,11 +37,15 @@ public class TableListActivity extends BaseActivity {
     ViewPager viewPager;
     @BindView(R.id.tv_back)
     TextView tvBack;
+    @BindView(R.id.et_search)
+    EditText etSearch;
 
     private MyViewPagerAdapter myViewPagerAdapter;
     private GridViewTableListAdapter mAdapter;
     private ArrayList<GridView> array;
     private static final float APP_PAGE_SIZE = 12.0f;
+
+    private TDForm[] exampleArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -92,7 +101,30 @@ public class TableListActivity extends BaseActivity {
 
             }
         });*/
-
+        etSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_SEARCH) {
+                    String str = textView.getText().toString();
+                    Log.d("TableListActivity", "搜索");
+                    if (getCurrentFocus() != null) {
+                        ((InputMethodManager) getSystemService(INPUT_METHOD_SERVICE))
+                                .hideSoftInputFromWindow(getCurrentFocus()
+                                                .getWindowToken(),
+                                        InputMethodManager.HIDE_NOT_ALWAYS);
+                        List<TDForm> examplelist = new ArrayList<TDForm>();
+                        for (int a = 0; a < exampleArray.length; a++) {
+                            TDForm t = exampleArray[a];
+                            if (t.FORM_NAME.contains(str)) {
+                                examplelist.add(t);
+                            }
+                        }
+                        initAdapter(examplelist);
+                    }
+                }
+                return false;
+            }
+        });
         tvBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -102,10 +134,10 @@ public class TableListActivity extends BaseActivity {
     }
 
     public void getData() {
-        int position=getIntent().getIntExtra("position",0);
-        appContext.tdURL=appContext.list_dept.get(position).getDIR_URL();
-        TDForm[] exampleArray=appContext.TDFormList.get(position);
-        appContext.temp=exampleArray;
+        int position = getIntent().getIntExtra("position", 0);
+        appContext.tdURL = appContext.list_dept.get(position).getDIR_URL();
+        exampleArray = appContext.TDFormList.get(position);
+        appContext.temp = exampleArray;
         Log.d("TableListActivity", "position:" + exampleArray);
         if (exampleArray != null && exampleArray.length > 0) {
             List<TDForm> examplelist = Arrays.asList(exampleArray);
@@ -149,5 +181,52 @@ public class TableListActivity extends BaseActivity {
                 }
             });
         }
+    }
+
+    public void initAdapter(List<TDForm> examplelist) {
+        final int PageCount = (int) Math.ceil(examplelist.size() / APP_PAGE_SIZE);
+        array = new ArrayList<GridView>();
+        for (int i = 0; i < PageCount; i++) {
+            mAdapter = new GridViewTableListAdapter(this, examplelist, i);
+            GridView gridView = new GridView(this);
+            gridView.setAdapter(mAdapter);
+            gridView.setNumColumns(3);
+            gridView.setGravity(Gravity.CENTER);
+            gridView.setVerticalSpacing(25);
+            gridView.setStretchMode(GridView.STRETCH_COLUMN_WIDTH);
+            gridView.setSelector(new ColorDrawable(Color.TRANSPARENT));
+            array.add(gridView);
+        }
+
+        //初始化圆点导航指示器
+        indicator.setLenght(PageCount);
+        indicator.setSelected(0);
+        indicator.draw();
+        myViewPagerAdapter = new MyViewPagerAdapter(this, array);
+        viewPager.setAdapter(myViewPagerAdapter);
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                //根据viewpager的改变修正圆点导航指示器
+                indicator.setSelected(position);
+                indicator.draw();
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    @OnClick(R.id.tv_cancel)
+    public void onClick() {
+        etSearch.setText("");
+        getData();
     }
 }
